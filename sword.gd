1efae6feee86ca02a56754423b3e73035f36192d
Lunetta21 <gabriel.iel.lunelli@gmail.com>
2025-02-04 09:43:45 -0300

    espada e escudo funcionadno
    espada e escudo funcionando e base do tilemap

--- sword.gd
+++ sword.gd
@@ -1,28 +1,49 @@
 extends Node2D
 
-var tween: Tween
-@onready var link = $"../link"  # Referência ao objeto "link"
+var rotating = false
+var rotation_speed = 25.0  
+var base_offset = deg_to_rad(-75)  
+var total_rotation = 0.0  
+var max_rotation = deg_to_rad(130)  
 
-func _ready():
-	tween = create_tween()  # Cria um novo Tween
+@onready var link = $"../link"  
+@onready var sprite = $Sprite2D  
+@onready var collision = $CollisionShape2D  
 
 func _physics_process(delta):
-	global_position = link.global_position  # Segue a posição do link
-	
-	# Faz o objeto olhar para o mouse
-	look_at_mouse()
+	global_position = link.global_position  
 
-	# Rotação adicional quando o botão esquerdo do mouse é pressionado
+	# Se o botão direito do mouse estiver pressionado, a espada não faz nada
+	if Input.is_action_pressed("mouse_right"):
+		rotating = false
+		total_rotation = 0.0
+		sprite.visible = false
+		collision.disabled = true
+		return
+
+	var target_angle = (get_global_mouse_position() - global_position).angle() + base_offset
+	
+	if rotating:
+		var rotation_amount = rotation_speed * delta
+		rotation += rotation_amount
+		total_rotation += abs(rotation_amount)  
+		
+		sprite.visible = true  
+		collision.disabled = false  
+		
+		if total_rotation >= max_rotation:
+			rotating = false
+			total_rotation = 0.0  
+	else:
+		rotation = target_angle  
+		sprite.visible = false  
+		collision.disabled = true  
+
+func _input(event):
+	# Se o botão direito do mouse estiver pressionado, o ataque não inicia
+	if Input.is_action_pressed("mouse_right"):
+		return
+
 	if Input.is_action_just_pressed("mouse_left"):
-		rotation += deg_to_rad(80)  # Converte 80 graus para radianos
-
-func look_at_mouse():
-	# Obtém a posição do mouse na tela
-	var mouse_position = get_global_mouse_position()
-	
-	# Calcula o ângulo entre a posição do personagem e a posição do mouse
-	var direction = (mouse_position - global_position).normalized()
-	var angle = direction.angle()
-	
-	# Aplica a rotação ao personagem com um offset de -90 graus
-	rotation = angle + deg_to_rad(-90)
+		rotating = true
+		total_rotation = 0.0  
